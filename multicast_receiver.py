4e9b42266f1aa15a2d6bb7666841b3dff1427482
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-05-25 21:01:22 +0200

    Update multicast_receiver.py

--- multicast_receiver.py
+++ multicast_receiver.py
@@ -20,18 +20,18 @@ def alive():
 
 
 def receive():
-	_thread.start_new_thread(alive, ())
+	_thread.start_new_thread( alive, () )
 	MCAST_GRP = '224.1.1.1'
 	MCAST_PORT = 5007
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+	sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP )
 	try:
-		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		sock.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
 	except AttributeError:
 		pass
-	sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32) 
-	sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
+	sock.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32 ) 
+	sock.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1 )
 
-	sock.bind((MCAST_GRP, MCAST_PORT))
+	sock.bind( ( MCAST_GRP, MCAST_PORT ) )
 
 	host = get_ip.get_lan_ip()
 
@@ -47,4 +47,4 @@ def receive():
 				ips[ data ] = time.time()
 				lock.release()
 		except socket.error:
-			print ('Expection')
+			print ( 'Expection' )
