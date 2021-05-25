1af146dd93857d55e1b040cb6b95a4035bd44637
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-05-26 00:17:55 +0200

    Update hepp.py

--- hepp.py
+++ hepp.py
@@ -1,11 +1,17 @@
 import TokenRing
+from omxplayer.player import OMXPlayer
+from pathlib import Path
 #import Measure
 videos = {
-	'floorLoop': '',
-	'tableComesIn': '',
-	'tableLoop': '',
-	'tableGoesOut': ''
+	'floorLoop': Path('./floorLoop.mp4' ),
+	'tableComesIn': Path('./tableComesIn.mp4' ),
+	'tableLoop': Path('./tableLoop.mp4' ),
+	'tableGoesOut': Path('./tableGoesOut.mp4' )
 }
+
+loop = OMXPlayer( videos.floorLoop, dbus_name='org.mpris.MediaPlayer2.loop' )
+hepp = OMXPlayer( videos.tableComesIn, dbus_name='org.mpris.MediaPlayer2.hepp' )
+hepp.pause()
 
 def compute_videos( starting = 0, total = 0 ):
 	# measure.distance()
