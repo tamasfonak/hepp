70969e3c151cd5fd0e661eb08d62ae0508bf84f4
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-05-25 23:22:39 +0200

    Update hepp.py

--- hepp.py
+++ hepp.py
@@ -1,6 +1,13 @@
 import TokenRing
 
-def compute_videos( starting=0, total=0 ):
+videos = {
+	'floorLoop': '',
+	'tableComesIn': '',
+	'tableLoop': '',
+	'tableGoesOut': ''
+}
+
+def compute_videos( starting = 0, total = 0 ):
 	return ( starting, total )
 
 TokenRing.token_sending.videos = compute_videos
