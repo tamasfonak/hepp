910bdc000701fa687cc87d5f2f9e4ab7d8dafad4
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-05-25 22:24:02 +0200

    Update multicast_sender.py

--- multicast_sender.py
+++ multicast_sender.py
@@ -7,8 +7,10 @@ def send():
 	host = get_ip.get_lan_ip()
 	MCAST_GRP = '224.1.1.1'
 	MCAST_PORT = 5007
+
 	sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP )
 	sock.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32 )
+
 	while True:
 		sock.sendto( host.encode(), ( MCAST_GRP, MCAST_PORT ) )
 		time.sleep( 3 )
