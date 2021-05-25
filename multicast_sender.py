96ea2d24db77103987c5b1a62b68b388c0b73fc2
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-05-25 20:59:36 +0200

    Update multicast_sender.py

--- multicast_sender.py
+++ multicast_sender.py
@@ -11,4 +11,4 @@ def send():
 	sock.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32 )
 	while True:
 		sock.sendto( host.encode(), ( MCAST_GRP, MCAST_PORT ) )
-		time.sleep(3)
+		time.sleep( 3 )
