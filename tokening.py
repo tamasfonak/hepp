fdeb904bbb015ce6a7a1da21737d18ef80c3433b
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-01 23:40:01 +0200

    Update tokening.py

--- tokening.py
+++ tokening.py
@@ -38,7 +38,7 @@ class HttpHandler( BaseHTTPRequestHandler ):
 	def _set_headers( self ):
 		self.send_response( 200 )
 		self.send_header( 'Content-type', 'application/json' )
-        	self.end_headers()
+		self.end_headers()
 	def do_HEAD( self ):
 		self._set_headers()
 	def do_GET( self ):
