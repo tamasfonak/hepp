f8866af02a0f89803ee53a21c73db87fde385061
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-01 23:17:30 +0200

    Update and rename token_listening.py to tokening.py

--- tokening.py
+++ tokening.py
@@ -1,7 +1,10 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.client import HTTPConnection
 import json
 import cgi
 import time
+import multicast
+import random
 
 class CallbackHTTPServer( HTTPServer ):
     def server_activate( self ):
@@ -53,3 +56,43 @@ def run():
     httpd = CallbackHTTPServer( ( '', 5000 ), HttpHandler ) 
     print ( 'Starting httpd on port: 5000')
     httpd.serve_forever()
+
+    
+
+
+params = { 'token': 1 }
+headers = { 
+	'Content-type': 'application/json', 
+	'Accept': 'text/plain' 
+}
+
+def compute_token( params ):
+	print( "Hasn't been defined!" )
+
+token = compute_token
+
+def set_token():
+	val = token( params )
+	print( 'set_token: ', val )
+	#params[ 'token' ] = val[ 0 ]
+
+def connect():
+	while len( multicast.ips ) < 1:
+		pass # Ez a ciklus fut mi'g nincs ma'sik ge'pa a ha'lo'zaton. (Ha a saja't IP-t kiza'rom)
+	try:
+		multicast.lock.acquire()
+		connection = HTTPConnection( random.choice( list( multicast.ips.keys() ) ), 5000, timeout=10 )
+		multicast.lock.release()
+		connection.request( "POST", "/", json.dumps( params ), headers )
+		response = connection.getresponse()
+		return True
+	except multicast.socket.error:
+		return connect()
+
+def send_token():
+	set_token()
+	if connect() == False:
+		print( "Something went wrong" )
+	else:
+		params[ 'token' ] = 0
+	return True
