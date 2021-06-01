d39858859bc99fbeba677df420e86865f2712a67
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-01 23:25:31 +0200

    Update tokening.py

--- tokening.py
+++ tokening.py
@@ -83,8 +83,8 @@ def connect():
 		multicast.lock.acquire()
 		connection = HTTPConnection( random.choice( list( multicast.ips.keys() ) ), 5000, timeout=10 )
 		multicast.lock.release()
-		connection.request( "POST", "/", json.dumps( params ), headers )
-		response = connection.getresponse()
+		#connection.request( "POST", "/", json.dumps( params ), headers )
+		#response = connection.getresponse()
 		return True
 	except multicast.socket.error:
 		return connect()
