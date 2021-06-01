2955c4f725e3edcb07e4edaef3c3f2ab556167c7
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-01 22:57:29 +0200

    Update hepp.py

--- hepp.py
+++ hepp.py
@@ -1,7 +1,11 @@
-import TokenRing
+
 from omxplayer.player import OMXPlayer
 from pathlib import Path
+import _thread
+import token_listening
+import token_sending
 #import Measure
+
 videos = {
 	'floorLoop': Path( '/home/pi/hepp_videos/ures.mp4' ),
 	'tableComesIn': Path( '/home/pi/hepp_videos/tabla_jon.mp4' ),
@@ -17,5 +21,18 @@ def compute_token( params ):
 	print( params )
 	return ( params )
 
-TokenRing.token_sending.videos = compute_token
-TokenRing.start()
+def main_route():
+	text = request.form[ 'token' ]
+	if ( text == '1' ):
+		token_sending.params[ 'token' ] = 1
+		#token_sending.params[ 'distance' ] = request.form[ 'distance' ]
+		#token_sending.params[ 'total' ] = int( request.form[ 'total' ] )
+	_thread.start_new_thread( token_sending.send_token, () )
+	return True
+
+token_sending.videos = compute_token
+_thread.start_new_thread( token_sending.multicast.receive, () )
+_thread.start_new_thread( token_sending.multicast.send, () )
+_thread.start_new_thread( token_listening.run, () )
+if ( token_sending.params[ 'token' ] == 1 ):
+	token_sending.send_token()
