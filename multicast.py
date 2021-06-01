812ada346cf345be36aa4c885a400b5f9b156150
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-01 15:54:21 +0200

    Update multicast.py

--- multicast.py
+++ multicast.py
@@ -41,10 +41,10 @@ def receive():
 	while True:
 		try:
 			data, addr = sock.recvfrom( 1024 )
-			if data.decode() != host:
-				lock.acquire()
-				ips[ data ] = time.time()
-				lock.release()
+			#if data.decode() != host: # Saja't IP-k kiza'rom?
+			lock.acquire()
+			ips[ data ] = time.time()
+			lock.release()
 		except socket.error:
       			print( binascii.hexlify( data ) )
 
