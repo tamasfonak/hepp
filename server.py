204e09869f8ab240f37641e7355b6391509f1e81
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-01 22:10:28 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,48 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import cgi
 import time
 
 hostName = "localhost"
-hostPort = 9000
-
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
+hostPort = 5000
+class Server(BaseHTTPRequestHandler):
+    def _set_headers(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
-        self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
-
-myServer = HTTPServer((hostName, hostPort), MyServer)
-print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
-
-try:
-    myServer.serve_forever()
-except KeyboardInterrupt:
-    pass
-
-myServer.server_close()
-print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
+        
+    def do_HEAD(self):
+        self._set_headers()
+        
+    # GET sends back a Hello world message
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}))
+        
+    # POST echoes the message adding a JSON field
+    def do_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        
+        # refuse to receive non-json content
+        if ctype != 'application/json':
+            self.send_response(400)
+            self.end_headers()
+            return
+            
+        # read the message and convert it into a python dictionary
+        length = int(self.headers.getheader('content-length'))
+        message = json.loads(self.rfile.read(length))
+        
+        # add a property to the object, just to mess with data
+        message['received'] = 'ok'
+        
+        # send the message back
+        self._set_headers()
+        self.wfile.write(json.dumps(message))
+        
+def run( server_class=HTTPServer, handler_class=Server, port=5000 ):
+    server_address = ( '', port )
+    httpd = server_class( server_address, handler_class )
+    
+    print ( 'Starting httpd on port: ',  port )
+    httpd.serve_forever()
