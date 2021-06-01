015daef12d814920ba535decaabd4989db2df6ab
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-01 15:02:20 +0200

    Update hepp.py

--- hepp.py
+++ hepp.py
@@ -3,10 +3,10 @@ from omxplayer.player import OMXPlayer
 from pathlib import Path
 #import Measure
 videos = {
-	'floorLoop': Path( './floorLoop.mp4' ),
-	'tableComesIn': Path( './tableComesIn.mp4' ),
-	'tableLoop': Path( './tableLoop.mp4' ),
-	'tableGoesOut': Path( './tableGoesOut.mp4' )
+	'floorLoop': Path( './ures.mp4' ),
+	'tableComesIn': Path( './tabla_jon.mp4' ),
+	'tableLoop': Path( './ures_tabla.mp4' ),
+	'tableGoesOut': Path( './tabla_ki.mp4' )
 }
 
 loop = OMXPlayer( videos[ 'floorLoop' ], dbus_name='org.mpris.MediaPlayer2.loop' )
