c1ca6c0f77aef7080d07d71f5e83c6116f74a1ba
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-01 23:39:08 +0200

    Update tokening.py

--- tokening.py
+++ tokening.py
@@ -9,56 +9,57 @@ import random
 params = { 'token': 1 }
 
 class CallbackHTTPServer( HTTPServer ):
-    def server_activate( self ):
-        self.RequestHandlerClass.pre_start()
-        HTTPServer.server_activate( self )
-        self.RequestHandlerClass.post_start()
+	def server_activate( self ):
+		self.RequestHandlerClass.pre_start()
+		HTTPServer.server_activate( self )
+		self.RequestHandlerClass.post_start()
 
-    def server_close( self ):
-        self.RequestHandlerClass.pre_stop()
-        HTTPServer.server_close( self )
-        self.RequestHandlerClass.post_stop()
+	def server_close( self ):
+		self.RequestHandlerClass.pre_stop()
+		HTTPServer.server_close( self )
+		self.RequestHandlerClass.post_stop()
 
 class HttpHandler( BaseHTTPRequestHandler ):
-    @classmethod
-    def pre_start(cls):
-        print ('Before calling socket.listen()')
-    @classmethod
-    def post_start(cls):
-	if ( params[ 'token' ] == 1 ):
-		send_token()
-    @classmethod
-    def pre_stop(cls):
-        print ('Before calling socket.close()')
-    @classmethod
-    def post_stop(cls):
-        print ('After calling socket.close()')
+	@classmethod
+	def pre_start(cls):
+		print ('Before calling socket.listen()')
+
+	@classmethod
+	def post_start(cls):
+		if ( params[ 'token' ] == 1 ):
+			send_token()
+	@classmethod
+	def pre_stop(cls):
+		print ('Before calling socket.close()')
+	@classmethod
+	def post_stop(cls):
+		print ('After calling socket.close()')
         
-    def _set_headers( self ):
-        self.send_response( 200 )
-        self.send_header( 'Content-type', 'application/json' )
-        self.end_headers()
-    def do_HEAD( self ):
-        self._set_headers()
-    def do_GET( self ):
-        self._set_headers()
-        self.wfile.write( json.dumps( { 'hello': 'world', 'received': 'ok' } ).encode() )
-    def do_POST( self ):
-        ctype, pdict = cgi.parse_header( self.headers.getheader( 'content-type' ) )
-        if ctype != 'application/json':
-            self.send_response( 400 )
-            self.end_headers()
-            return
-        length = int( self.headers.getheader( 'content-length' ) )
-        message = json.loads( self.rfile.read( length ) )
-        message[ 'received' ] = 'ok'
-        self._set_headers()
-        self.wfile.write( json.dumps( message ).encode() )
+	def _set_headers( self ):
+		self.send_response( 200 )
+		self.send_header( 'Content-type', 'application/json' )
+        	self.end_headers()
+	def do_HEAD( self ):
+		self._set_headers()
+	def do_GET( self ):
+		self._set_headers()
+		self.wfile.write( json.dumps( { 'hello': 'world', 'received': 'ok' } ).encode() )
+	def do_POST( self ):
+		ctype, pdict = cgi.parse_header( self.headers.getheader( 'content-type' ) )
+		if ctype != 'application/json':
+			self.send_response( 400 )
+			self.end_headers()
+			return
+		length = int( self.headers.getheader( 'content-length' ) )
+		message = json.loads( self.rfile.read( length ) )
+		message[ 'received' ] = 'ok'
+		self._set_headers()
+		self.wfile.write( json.dumps( message ).encode() )
         
 def listen():
-    httpd = CallbackHTTPServer( ( '', 5000 ), HttpHandler ) 
-    print ( 'Starting httpd on port: 5000')
-    httpd.serve_forever()
+	httpd = CallbackHTTPServer( ( '', 5000 ), HttpHandler ) 
+	print ( 'Starting httpd on port: 5000')
+	httpd.serve_forever()
 
 
 headers = { 
