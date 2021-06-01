4036cf0b07d8b3a912224beb29aab67892448bba
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-01 22:25:43 +0200

    Update token_listening.py

--- token_listening.py
+++ token_listening.py
@@ -3,42 +3,27 @@ import json
 import cgi
 import time
 
-hostName = "localhost"
-hostPort = 5000
 class Server(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+    def _set_headers( self ):
+        self.send_response( 200 )
+        self.send_header( 'Content-type', 'application/json' )
         self.end_headers()
-        
-    def do_HEAD(self):
+    def do_HEAD( self ):
         self._set_headers()
-        
-    # GET sends back a Hello world message
-    def do_GET(self):
+    def do_GET( self ):
         self._set_headers()
-        self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}))
-        
-    # POST echoes the message adding a JSON field
-    def do_POST(self):
-        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-        
-        # refuse to receive non-json content
+        self.wfile.write( json.dumps( { 'hello': 'world', 'received': 'ok' } ) )
+    def do_POST( self ):
+        ctype, pdict = cgi.parse_header( self.headers.getheader( 'content-type' ) )
         if ctype != 'application/json':
-            self.send_response(400)
+            self.send_response( 400 )
             self.end_headers()
             return
-            
-        # read the message and convert it into a python dictionary
-        length = int(self.headers.getheader('content-length'))
-        message = json.loads(self.rfile.read(length))
-        
-        # add a property to the object, just to mess with data
-        message['received'] = 'ok'
-        
-        # send the message back
+        length = int( self.headers.getheader( 'content-length' ) )
+        message = json.loads( self.rfile.read( length ) )
+        message[ 'received' ] = 'ok'
         self._set_headers()
-        self.wfile.write(json.dumps(message))
+        self.wfile.write( json.dumps( message ) )
         
 def run( server_class=HTTPServer, handler_class=Server, port=5000 ):
     server_address = ( '', port )
