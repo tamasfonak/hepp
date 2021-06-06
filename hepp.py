e92c74b2ac40c4d95d02e6a46b6dea893554aec9
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-06 12:47:44 +0200

    Update hepp.py

--- hepp.py
+++ hepp.py
@@ -5,46 +5,63 @@ import _thread
 import tokening
 #import Measure
 
-logging.basicConfig( level = logging.DEBUG )
+logging.basicConfig( level = logging.INFO )
 
 videos = {
-	'floorLoop': Path( '/home/pi/hepp_videos/ures.mp4' ),
-	'tableComesIn': Path( '/home/pi/hepp_videos/tabla_jon.mp4' ),
-	'tableLoop': Path( '/home/pi/hepp_videos/ures_tabla.mp4' ),
-	'tableGoesOut': Path( '/home/pi/hepp_videos/tabla_ki.mp4' )
+        'floorLoop': '/home/pi/hepp_videos/ures.mp4',
+        'tableComesIn': '/home/pi/hepp_videos/tabla_jon.mp4',
+        'tableLoop': '/home/pi/hepp_videos/ures_tabla.mp4',
+        'tableGoesOut': '/home/pi/hepp_videos/tabla_ki.mp4'
 }
 
-loop = OMXPlayer( videos[ 'floorLoop' ], args = [ '--no-osd', '--loop', '--layer', '0' ], dbus_name = 'org.mpris.MediaPlayer2.loop', pause = True )
-hepp = OMXPlayer( videos[ 'tableComesIn' ], args = [ '--no-osd', '--layer', '1' ], dbus_name='org.mpris.MediaPlayer2.hepp', pause = True )
+loop = OMXPlayer( Path( videos[ 'floorLoop' ] ), args = [ '--no-osd', '--loop', '--layer', '0' ], dbus_name = 'org.mpris.MediaPlayer2.loop' )
 
 isHepp = False
 
-def compute_video():
-        loop.play()
-        tokening.time.sleep( 1 )
-        hepp.play()
-        loop.load( videos[ 'tableLoop' ], pause = True )
+def compute_video( heppFile, loopFile = False ):
+
+        hepp = OMXPlayer( Path( heppFile ), args = [ '--no-osd', '--layer', '1' ], dbus_name='org.mpris.MediaPlayer2.hepp' )
+
         isHepp = True;
+
+        if loopFile:
+                loop.load( Path( loopFile ) )
+
+        while hepp.position() < 1:
+                pass
+
+        if loopFile:
+                loop.load( Path( loopFile ) )
+
         while hepp.duration() > hepp.position() + 0.5:
-                pass #print( hepp.position() )
-        loop.play()
-        hepp.hide_video() # nem mukodik...
-        hepp.pause()
+                pass
+
+        hepp.stop()
+
         isHepp = False;
+
         tokening.params[ 'token' ] = 1
+
         return True
-	
+
 def compute_token( params ):
-	if params[ 'token' ] == 1 and isHepp == False:
-		_thread.start_new_thread( compute_video, () )
-	return ( params )
+        if params[ 'token' ] == 1 and isHepp == False:
+                _thread.start_new_thread( compute_video, () )
+        return ( params )
 
 tokening.set_token = compute_token
 _thread.start_new_thread( tokening.multicast.receive, () )
 _thread.start_new_thread( tokening.multicast.send, () )
 _thread.start_new_thread( tokening.listen, () )
 
-compute_video()
+
+tokening.time.sleep( 3 )
+compute_video( videos[ 'tableComesIn' ], videos[ 'tableLoop' ] )
+
+tokening.time.sleep( 3 )
+
+compute_video( videos[ 'tableGoesOut' ], videos[ 'floorLoop' ] )
 
 while True:
         pass
+
