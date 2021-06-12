79ec850968e06c9277e9a1f3fea3c8fc00d4f4aa
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-12 22:44:45 +0200

    Update get_ip.py

--- get_ip.py
+++ get_ip.py
@@ -15,7 +15,7 @@ if os.name != "nt":
 def get_lan_ip():
     	ip = socket.gethostbyname( socket.gethostname() )
     	if ip.startswith( "127." ) and os.name != "nt":
-        	interfaces = [ "eth0", "eth1", "eth2", "wlan0", "wlan1", "wifi0", "ath0", "ath1", "ppp0" ]
+        	interfaces = [ "wlan0", "wlan1", "wifi0", "ath0", "ath1", "ppp0", "eth0", "eth1", "eth2" ]
         	for ifname in interfaces:
             		try:
                 		ip = get_interface_ip( ifname )
