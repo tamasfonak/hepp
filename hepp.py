c22ef828a50e35a1050f407cd9ebad1725bf8533
tamasfonak <berces.tamas@gmail.com>
2021-06-22 14:37:23 +0100

    play_hepp improve

--- hepp.py
+++ hepp.py
@@ -17,16 +17,16 @@ videos = {
 loop = OMXPlayer( Path( videos[ 'floorLoop' ] ), args = [ '--no-osd', '--loop', '--layer', '0' ], dbus_name = 'org.mpris.MediaPlayer2.loop' )
 
 def play_hepp( heppFile, loopFile = False ):
-	hepp = OMXPlayer( Path( heppFile ), args = [ '--no-osd', '--layer', '1', '--win', '0,0,1920,1080', '--alpha', '32' ], dbus_name='org.mpris.MediaPlayer2.hepp' )
+	hepp = OMXPlayer( Path( heppFile ), args = [ '--no-osd', '--layer', '1', '--win', '0,0,1920,1080', '--alpha', '0' ], dbus_name='org.mpris.MediaPlayer2.hepp' )
 	while hepp.position() < 1:
 		alpha = int( hepp.position() * 255 )
-		if alpha < 255 and alpha > 1:
+		if alpha < 255 and alpha > 0:
 			hepp.set_alpha( alpha )
 	if loopFile:
 		loop.load( Path( loopFile ) )
-	while hepp.duration() > hepp.position() + 1:
+	while hepp.duration() > hepp.position() + 0.2:
 		alpha = int( ( hepp.duration() - hepp.position() ) * 255 )
-		if alpha < 255 and alpha > 1:
+		if alpha < 255 and alpha > 0:
 			hepp.set_alpha( alpha )
 	tokening.params[ 'token' ] = 1
 	return True
