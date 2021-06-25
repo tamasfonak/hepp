d405871d7ccdc54f17fb984f3410fd79e2890bf1
tamasfonak <berces.tamas@gmail.com>
2021-06-25 16:38:51 +0100

    new multicast

---
+++ intruder.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings( False )
+GPIO.setmode( GPIO.BCM )
+GPIO.setup( 17, GPIO.IN )
+
+while True:
+	
+	i = GPIO.input( 17 )
+	if i==1:
+		print( 'Intruder' )
+	time.sleep( 0.1 )
+	print( '' )
--- multicast.py
+++ multicast.py
@@ -6,8 +6,9 @@ import _thread
 import get_ip
 import json
 
-ips = {}
-status = 'waiting' # processing, passing
+alive = {}
+status = {}
+now = 'passing'
 
 lock = threading.Lock()
 
@@ -15,22 +16,14 @@ MCAST_GRP = '224.1.1.1'
 MCAST_PORT = 5007
 
 def call_hepp():
-	status = "processing"
-	print( 'hepp' );
-	time.sleep( 3 )
-
-def alive():
-	while True:
-		for ip in ips.keys():
-			lock.acquire()
-			if ( time.time() - ips[ ip ] ) > 5:
-				ips.pop( ip )
-			lock.release()
-		time.sleep( 5 )
+	global now
+	now = 'processing'
+	print( 'HEPP: ', now );
+	time.sleep( 5 )
+	now = 'passing'
+	return True
 
 def receive():
-	#_thread.start_new_thread( alive, () )
-	
 	host = get_ip.get_lan_ip()
 
 	sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP )
@@ -40,34 +33,48 @@ def receive():
 	except AttributeError as error:
 		print ( 'AttributeError: ', error )
 
-	sock.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32 ) 
+	sock.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32 )
 	sock.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1 )
 	sock.bind( ( MCAST_GRP, MCAST_PORT ) )
 	sock.setsockopt( socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton( host ) )
 	sock.setsockopt( socket.SOL_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton( MCAST_GRP ) + socket.inet_aton( host ) )
-	
+
 	while True:
+		global now
 		try:
-			data, ( addr, port ) = sock.recvfrom( 1024 )
-			lock.acquire()
-			ips[ addr ][ 'lastRecvTime' ] = time.time()
-			ips[ addr ][ 'status' ] = status
-			print( ips )
-			for ip in ips.keys():
-				if ( time.time() - ips[ ip ][ 'lastRecvTime' ] ) > 5:
-					ips.pop( ip )
-			lock.release()
+			sta, ( addr, port ) = sock.recvfrom( 1024 )
+
+			#lock.acquire()
+
+			if addr != host:
+				alive[ addr ] = time.time()
+				status[ addr ] = sta.decode()
+			print( 'Received: ', sta.decode() )
+			now = sta.decode()
+			for ip in alive.keys():
+				if ( time.time() - alive[ ip ] ) > 5:
+					alive.pop( ip )
+
+			#lock.release()
+
 		except socket.error:
       			print( 'socket.error: ', binascii.hexlify( data ) )
 
 def send():
 	host = get_ip.get_lan_ip()
-	
+
 	sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP )
 	sock.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32 )
 	sock.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton( host ) )
 
 	while True:
-		#print( 'sendto:', host )
-		sock.sendto( status.encode(), ( MCAST_GRP, MCAST_PORT ) )
+		global now
+		for ip in status.keys():
+			if now == 'pass' and status[ ip ] == 'processing':
+				now = 'waiting'
+		if now == 'passing':
+			_thread.start_new_thread( call_hepp, () )
+
+		sock.sendto( now.encode(), ( MCAST_GRP, MCAST_PORT ) )
+		print( 'Sent: ', now ) 
 		time.sleep( 1 )
