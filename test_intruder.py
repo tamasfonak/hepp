14c903a507b520f6eae635059aa4bae0c090b3a9
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-29 01:07:48 +0200

    Update test_intruder.py

--- test_intruder.py
+++ test_intruder.py
@@ -4,5 +4,6 @@ import _thread
 #_thread.start_new_thread( measure.start, () ) 
 
 while True:
-	print( measure.intruder() )
+	if measure.intruder > 5:
+		print( 'intruder' )
 	measure.time.sleep( 1 )
