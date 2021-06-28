d0fb4b065ba07210b5e078bbdc9883408062bd7e
Tamas Berces <tamasfonak@users.noreply.github.com>
2021-06-29 00:57:22 +0200

    Update test_intruder.py

--- test_intruder.py
+++ test_intruder.py
@@ -1,8 +1,8 @@
 import measure
 import _thread 
 
-_thread.start_new_thread( measure.start, () ) 
+#_thread.start_new_thread( measure.start, () ) 
 
 while True:
 	print( measure.intruder() )
-	measure.time.sleep( 0.1 )
+	measure.time.sleep( 1 )
